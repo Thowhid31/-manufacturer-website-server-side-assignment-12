37eca4e05d45657f4c9440e45fd58f34b2ca2999
Thowhid31 <thowhidantor16@gmail.com>
2022-05-22 22:37:36 +0600

    finally implement one by one product

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jqvdo.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const productsCollection = client.db('computer_mechanism').collection('products');
+
+        app.get('/product', async(req, res) =>{
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products)
+        })
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id
+                ;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.send(product);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.log('Hello CM'))
 
 app.get('/', (req, res) => {
-  res.send('Hello Fom Computer Mechanism!')
+  res.send('Hello From Computer Mechanism!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Computer listening on port ${port}`)
 })
