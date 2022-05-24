ae9593173bdba1cdf4823777ad3cc10ac0190b09
Thowhid31 <thowhidantor16@gmail.com>
2022-05-24 09:55:19 +0600

    added admin panel

--- index.js
+++ index.js
@@ -13,6 +13,23 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: 'Unauthorized Access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'Forbidden Access' })
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
+
+
 async function run() {
     try {
         await client.connect();
@@ -25,6 +42,11 @@ async function run() {
             const cursor = productsCollection.find(query);
             const products = await cursor.toArray();
             res.send(products)
+        });
+
+        app.get('/user', verifyJWT, async(req, res)=>{
+            const users = await userCollection.find().toArray();
+            res.send(users);
         })
 
         app.put('/user/:email', async (req, res) => {
@@ -40,6 +62,7 @@ async function run() {
             res.send({result, token});
         })
 
+        
         app.get('/product/:id', async (req, res) => {
             const id = req.params.id
                 ;
